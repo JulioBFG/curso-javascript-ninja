b595c80df5103f5b250b72803cd76b2f22179a40
juliolopes.bfg@gmail.com <juliocesar@pop-os.localdomain>
2020-04-28 16:20:21 -0300

    Add: new challenge

--- challenge-14/challenge-14.js
+++ challenge-14/challenge-14.js
@@ -1,6 +1,8 @@
-/*
-Envolva todo o código desse desafio em uma IIFE.
-*/
+(function() {
+    
+
+//Envolva todo o código desse desafio em uma IIFE.
+
 
 /*
 Crie um array chamado numberObjects. Esse array deve ter 10 elementos. Cada
@@ -10,14 +12,23 @@ Os números devem ser de 1 a 10.
 Mostre esse array no console.
 */
 console.log( 'Number Objects Array:' );
-// ?
 
+var numberObjects = [];
+for (let i = 0; i <= 10; i++) {
+  numberObjects.push({number : i})
+}
+console.log(numberObjects);
 /*
 Crie um array chamado `justNumbers`, que terá como elementos somente os
 números do array criado acima. Mostre esse novo array no console.
 */
 console.log( '\nJust Numbers:' );
 // ?
+var justNumbers = numberObjects.map(function(item){
+  return item.number
+});
+
+console.log(justNumbers)
 
 /*
 Crie um novo array chamado `justMod2Or3`, que receberá do array criado acima
@@ -27,6 +38,11 @@ no console.
 console.log( '\nJust module of division by 2 or 3:' );
 // ?
 
+var justMod2Or3 = justNumbers.filter(function(item){
+  return item % 2 === 0 || item % 3 === 0;
+})
+
+console.log(justMod2Or3);
 /*
 Declare uma variável chamada operation que receba, do array criado acima,
 um valor reduzido pela seguinte operação:
@@ -36,7 +52,11 @@ O cálculo deve começar com zero.
 Mostre o resultado no console.
 */
 console.log( '\nOperation:' );
-// ?
+var operation = justMod2Or3.reduce(function(acumulado, atual){
+  return( acumulado + 1) * atual;
+},0 );
+
+console.log(operation);
 
 /*
 Faça o mesmo cálculo passado acima, mas começando do último item para o
@@ -46,6 +66,11 @@ console.
 console.log( '\nOperation 2:' );
 // ?
 
+var operation2 = justMod2Or3.reduceRight(function(acumulado, atual){
+  return(acumulado + 1) * atual;
+},0 );
+
+console.log(operation2);
 /*
 Crie um array chamado `name`. Cada elemento desse array deve ser uma sílaba
 do seu nome. Vamos reduzir esse array, juntando todas as sílabas, mas usando
@@ -57,18 +82,31 @@ falada, como se você estivesse falando em código xD
 console.log( '\nSeu nome na língua do "P":' );
 // ?
 
+var name  = ['Ju','lio'];
+
+var reduceP = name.reduce(function(acumulado,atual){
+  return acumulado +'P' + atual;
+},'');
+console.log(reduceP);
+
+
+
 /*
 Crie uma variável chamada `inversedName`, que reduzirá o array em uma string
 e atribuirá o seu nome invertido (usando o array criado acima).
 */
 console.log( '\nInversed Name:' );
 // ?
-
+var inversedName = name.reduceRight(function(acumulado,atual) {
+  return acumulado + atual;
+});
+console.log(inversedName)
 /*
 Mostre no console o array `numberObjects`.
 */
 console.log( '\nNumber objects' );
 // ?
+console.log(numberObjects );
 
 /*
 Verifique se existem em algum índice de numberObjects um objeto ìgual a
@@ -80,7 +118,13 @@ Consegue prever o resultado? Deixe uma mensagem no console tentando explicar
 o que acontece ;)
 */
 console.log( '\nExiste um { number: 2 } em numberObjects?' );
-// ?
+var obj = numberObjects[1]
+if (numberObjects.indexOf(obj) > -1){
+  console.log('Existe um { number: 2 } em numberObjects')
+}
+else{
+  console.log(' nao existe um { number: 2 } em numberObjects')
+}
 
 /*
 Fazendo o mesmo do exercício acima, mas começando a buscar do último índice,
@@ -88,10 +132,22 @@ será que obtemos um resultado diferente? Faça a busca a partir do índice 2.
 */
 console.log( '\nE buscando a partir do último índice, o { number: 2 } existe?' );
 // ?
+if (numberObjects.lastIndexOf(obj,2) > -1){
+  console.log('Existe um { number: 2 } em numberObjects')
+}
+else{
+  console.log(' nao existe um { number: 2 } em numberObjects')
+}
 
 /*
 Verifique se `justMod2Or3` é um array. Se for, mostre-o no console, no
 formato de String.
 */
 console.log( '\njustMod2Or3 é um array? Se for, a representação dele em String é:' );
-// ?
+  if(Array.isArray(justMod2Or3)){
+    console.log(justMod2Or3.toString());
+  }
+  else {
+    console.log('nao e array');
+  }
+}())
